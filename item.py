3c6f749a943a339c2b7781ecb72b047eb19e5129
Luconha <lucaas.oliveira0254@gmail.com>
2024-05-14 08:02:11 -0300

    add: item/player: init; main: tela base

--- item.py
+++ item.py
@@ -1,6 +1,6 @@
 import pygame
 
-class Player:
+class Item:
 
     def __init__(self, imagem, largura, altura, pos_x, pos_y,):
         self.imagem = pygame.image.load(imagem)
--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 import pygame
 from player import Player
+from item import Item
 
+pygame.init()
+tela = pygame.display.set_mode((800,500))
+pygame.display.set_caption("Lorem ipsum")
+
+clock = pygame.time.Clock()
+
+running = True
+while running:
+    tela.fill((255,0,0))
+    tela.fill((0,255,0))
+    
+    
+    
+    pygame.display.update()
+
+    clock.tick(60)
